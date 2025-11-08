4cb77530d941753c49992a82c01578b29e4ee52c
Rifat132445 <Rifat14312a@gmail.com>
2025-11-08 20:24:17 +0600

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -57,7 +57,7 @@ module.exports = {
       const totalCommands = commands.size;
       msg += `\n\n⭔Bot has ${totalCommands} commands\n⭔Type ${prefix}𝐡𝐞𝐥𝐩 <𝚌𝚘𝚖𝚖𝚊𝚗𝚍 𝚗𝚊𝚖𝚎> to learn Usage.\n`;
       msg += ``;
-      msg += `\n╭─✦ADMIN: Rifat Ahmed 🧃\n├‣ FACEBOOK\n╰‣:https://m.me/rifat.ahmed.37800`; // customize this section if needed
+      msg += `\n╭─✦ADMIN: Xit Zefox  🧃\n├‣ FACEBOOK\n╰‣:https://m.me/xitzefox48`; // customize this section if needed
 
       try {
         const hh = await message.reply({ body: msg });
